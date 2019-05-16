c48a07775e02aacd70f43310e939c5e8f416841b
Raúl Cisternas <r.cisternas.w@gmail.com>
2019-05-15 21:45:21 -0400

    REFACT: user routes

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,8 @@
 'use strict'
 
-const router         = require('express').Router();
-const userController = require('../controllers/userController');
+const router          = require('express').Router();
+const user_controller = require('../controllers/userController');
 
-router.get('/user/:id', userController.getUser);
+router.get('/user/:id', user_controller.get_user);
 
 module.exports = router;
